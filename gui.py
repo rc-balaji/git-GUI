36baf5d7565495ad1e2853759f4e45613ee7db41
rc-balaji <balajic@drngpit.ac.in>
2023-09-24 20:56:28 +0530

    Finish

--- gui.py
+++ gui.py
@@ -1,25 +1,34 @@
-import subprocess
+import os
+import git
 
-while True:
-    # Get user input
-    user_input = input("Git Bash > ")
+# Get user input for the root directory path
+root_dir = input("Enter the root directory path: ")
 
-    # Exit the loop if the user enters "exit"
-    if user_input.lower() == "exit":
-        break
+# Get user input for the branch name
+branch_name ="main"#input("Enter the branch name: ")
 
-    # Run the Git Bash command
-    try:
-        result = subprocess.run(
-            user_input,
-            shell=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True
-        )
+# Get user input for the Git repository URL
+git_url = input("Enter the Git repository URL: ")
 
-        # Print the command's standard output and error
-        print(result.stdout)
-        print(result.stderr)
-    except Exception as e:
-        print("Error:", e)
+# Initialize a Git repository in the root directory
+repo = git.Repo.init(root_dir)
+
+try:
+    # Add files to the staging area
+    repo.git.add(A=True)
+
+    # Commit the changes
+    repo.git.commit('-m', 'Auto commit')
+
+    # Rename the branch to the specified branch name
+    repo.git.branch('-M', branch_name)
+
+    # Add the remote repository with the provided URL
+    repo.create_remote('custom_remote', git_url)
+
+    # Push to the specified branch on the custom remote repository
+    repo.git.push('custom_remote', branch_name)
+
+    print(f"Pushed to the {branch_name} branch on your custom remote repository.")
+except Exception as e:
+    print("Error:", e)
